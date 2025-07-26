91e8713639cf0b1b99405b779be6808455d5ec92
tickBit <marjukosk@gmail.com>
2025-07-26 18:09:55 +0300

    Add yet two spheres

--- Point3D.py
+++ Point3D.py
@@ -6,11 +6,15 @@ class Point3D:
         self.z = z
 
     def project(self):
+        
         # Simple perspective projection
         factor = 200 / (self.z + 4)
         x_proj = int(self.x * factor + 400)
         y_proj = int(self.y * factor + 300)
         return (x_proj, y_proj)
+    
+    def getY(self):
+        return self.y
     
     def getZ(self):
         return self.z
